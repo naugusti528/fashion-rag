dbd2754a22218eaf9da3303a6c15562895c6c1e1
naugusti528 <naugusti@ucsc.edu>
2025-07-06 19:14:56 +0000

    main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+print("bruv")
+
+#this is to print in colors
+print("\033[m")
+## default color is ( [0m ), black - 30, red - 31, green - 32, yellow - 33, blue - 34, purple - 35, cyan - 36, white - 37
